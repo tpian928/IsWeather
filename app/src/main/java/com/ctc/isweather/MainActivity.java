4f2db00dd7a44c1536d28a3edda6666e8d0668e5
王怡飞 <TPIAN@wangyifeideMacBook-Air.local>
2015-07-12 16:25:13 +0800

    test

--- app/src/main/java/com/ctc/isweather/MainActivity.java
+++ app/src/main/java/com/ctc/isweather/MainActivity.java
@@ -2,6 +2,7 @@ package com.ctc.isweather;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity","test");
         setContentView(R.layout.activity_main);
     }
 
