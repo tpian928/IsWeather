1fb1c5191f9c50f4e35737681f9cefb8decea289
kebreathing <463720769@qq.com>
2015-07-13 11:13:12 +0800

    add test

---
+++ app/src/main/java/database/isweather/ctc/com/DatabaseOperations.java
@@ -0,0 +1,11 @@
+package database.isweather.ctc.com;
+
+import java.io.InputStream;
+
+/**
+ * 数据库连接接口
+ * Created by chris on 2015/7/13.
+ */
+public interface DatabaseOperations {
+    void importDB(String packageName,InputStream input);
+}
