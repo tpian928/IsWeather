f9bd5bee605d1f4b93940fca604e7e09f50b644d
kebreathing <463720769@qq.com>
2015-07-14 10:03:19 +0800

    add draw linechart
    包装好了画图类，传参数就可以直接画图

--- app/src/main/java/com/ctc/isweather/MainActivity.java
+++ app/src/main/java/com/ctc/isweather/MainActivity.java
@@ -1,19 +1,31 @@
 package com.ctc.isweather;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Button click;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("MainActivity","test");
+        Log.d("MainActivity", "test");
         setContentView(R.layout.activity_main);
+        click = (Button) findViewById(R.id.btnSkip);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ChartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
