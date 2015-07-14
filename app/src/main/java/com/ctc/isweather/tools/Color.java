b830a795b9cf1140b0d75e67b35a0ae84de177db
王怡飞 <TPIAN@wangyifeideMacBook-Air.local>
2015-07-14 15:06:52 +0800

    add Color

---
+++ app/src/main/java/com/ctc/isweather/tools/Color.java
@@ -0,0 +1,18 @@
+package com.ctc.isweather.tools;
+
+/**
+ * Created by TPIAN on 15/7/14.
+ */
+public class Color {
+
+    /**
+     * 通过给出的天气度数得到对应的色卡元素
+     * @param temp int类型的温度
+     * @return 16位颜色
+     */
+    public static String getColorByTemp(int temp){
+
+        return  null;
+    }
+
+}
