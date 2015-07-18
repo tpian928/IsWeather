3eb1582e76db0a8e4b5a3168c4ea71fcaba527bb
kebreathing <463720769@qq.com>
2015-07-18 09:09:36 +0800

    修改数据库

--- app/src/main/java/com/ctc/isweather/view/activity/MainActivity.java
+++ app/src/main/java/com/ctc/isweather/view/activity/MainActivity.java
@@ -1,15 +1,19 @@
 package com.ctc.isweather.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ctc.isweather.R;
 
 public class MainActivity extends Fragment{
+
+
 
     // each move, need to get the data from the internet
     static MainActivity newInstance(String cityname){
@@ -29,13 +33,27 @@ public class MainActivity extends Fragment{
         return view;
     }
 
+    private TextView city_tv;
+    private ImageView city_iv;
     /**
      * initialize the activity where we need to check whether the network work
      * @param bundle
      * @param view
      */
     public void init(Bundle bundle,View view){
-        TextView test = (TextView) view.findViewById(R.id.title_TextView);
-        test.setText(bundle.getString("name"));
+        city_tv = (TextView) view.findViewById(R.id.title_TextView);
+        city_tv.setText(bundle.getString("name"));
+
+        // set the main imageview of the certain city
+        city_iv = (ImageView) view.findViewById(R.id.city_imageView);
+        city_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // some differences between activities and fragments.
+                Intent intent = new Intent(getActivity(),TodayWeatherActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
