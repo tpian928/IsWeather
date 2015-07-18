0e3db27465419cb96b782f2b69fea933b09c5250
tpian928 <TPIAN@wangyifeideMacBook-Air.local>
2015-07-18 08:30:28 +0800

    定位

--- app/src/main/java/com/ctc/isweather/control/Location.java
+++ app/src/main/java/com/ctc/isweather/control/Location.java
@@ -4,4 +4,13 @@ package com.ctc.isweather.control;
  * Created by TPIAN on 15/7/16.
  */
 public class Location {
+
+    public static String getLocationCityName(){
+        String cityName = null;
+
+
+
+        return  cityName;
+    }
+
 }
