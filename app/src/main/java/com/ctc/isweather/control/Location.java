953e36d6b05f357a7be80da4a4783cdcbc219149
tpian928 <TPIAN@wangyifeideMacBook-Air.local>
2015-07-18 08:30:43 +0800

    Revert 0bf641f..2ad7464
    This rolls back to commit 0bf641f48f1acf40fbd9320a5d246237d3d8e9d2.

--- app/src/main/java/com/ctc/isweather/control/Location.java
+++ app/src/main/java/com/ctc/isweather/control/Location.java
@@ -4,13 +4,4 @@ package com.ctc.isweather.control;
  * Created by TPIAN on 15/7/16.
  */
 public class Location {
-
-    public static String getLocationCityName(){
-        String cityName = null;
-
-
-
-        return  cityName;
-    }
-
 }
