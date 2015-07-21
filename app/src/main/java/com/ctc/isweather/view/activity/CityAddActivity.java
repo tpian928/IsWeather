a5c67083a97effedad46254203540a990817d442
ClothoWong <wanghui9874@126.com>
2015-07-21 18:41:08 +0800

    搜索（虚拟机不能调试中文）+界面元素微调

--- app/src/main/java/com/ctc/isweather/view/activity/CityAddActivity.java
+++ app/src/main/java/com/ctc/isweather/view/activity/CityAddActivity.java
@@ -1,89 +1,51 @@
 package com.ctc.isweather.view.activity;
 
 import android.app.Activity;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.SimpleCursorAdapter;
 import com.ctc.isweather.R;
+;
 
-import java.lang.reflect.Field;
-
-public class CityAddActivity extends Activity {
-    private SearchView searchView;
+public class CityAddActivity extends Activity implements SearchView.OnQueryTextListener {
+    private SearchView search;
+    private ListView lv;
+    private String[] names=new String[]{"张三","李四","王五","王会"};//数据库里的城市名取出来，组成数组可以吗？
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_add_);
 
-        searchView = (SearchView) findViewById(R.id.searchview);
-        searchView.setIconifiedByDefault(false);
-        searchView.setSubmitButtonEnabled(true);
-        searchView.setQueryHint("add city");
-
-        try {
-            Field field = searchView.getClass().getDeclaredField("mSubmitButton");
-            field.setAccessible(true);
-            ImageView iv = (ImageView) field.get(searchView);
-            iv.setImageDrawable(this.getResources().getDrawable(R.drawable.add));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        /****************************************************************************************************************************************************
-         * 数据库中取数据
-         */
-        Cursor cursor = this.getTestCursor();
-
-        @SuppressWarnings("deprecation")
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-                R.layout.search_city_text_view, cursor, new String[] { "tb_name" },
-                new int[] { R.id.textview });
-        searchView.setSuggestionsAdapter(adapter);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextChange(String str) {
-                return false;
-            }
-            @Override
-            public boolean onQueryTextSubmit(String str) {
-                //Toast.makeText(CityAddActivity.this, str, Toast.LENGTH_SHORT).show();
-                //添加该城市到数据库
-                return false;
-            }
-        });
+        initview();
     }
 
-    //添加suggestion需要的数据，调试用
-    public Cursor getTestCursor() {
-        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(
-                this.getFilesDir() + "/my.db3", null);
-        Cursor cursor = null;
-        try {
-            String insertSql = "insert into tb_test values (null,?,?)";
-            db.execSQL(insertSql, new Object[] { "aa", 1 });
-            db.execSQL(insertSql, new Object[] { "ab", 2 });
-            db.execSQL(insertSql, new Object[] { "ac", 3 });
-            db.execSQL(insertSql, new Object[] { "ad", 4 });
-            db.execSQL(insertSql, new Object[] { "ae", 5 });
-            String querySql = "select * from tb_test";
-            cursor = db.rawQuery(querySql, null);
-        } catch (Exception e) {
-            String sql = "create table tb_test (_id integer primary key autoincrement,tb_name varchar(20),tb_age integer)";
-            db.execSQL(sql);
-            String insertSql = "insert into tb_test values (null,?,?)";
-            db.execSQL(insertSql, new Object[] { "aa", 1 });
-            db.execSQL(insertSql, new Object[] { "ab", 2 });
-            db.execSQL(insertSql, new Object[] { "ac", 3 });
-            db.execSQL(insertSql, new Object[] { "ad", 4 });
-            db.execSQL(insertSql, new Object[] { "ae", 5 });
-            String querySql = "select * from tb_test";
-            cursor = db.rawQuery(querySql, null);
+    public void initview()
+    {
+        search =(SearchView)findViewById(R.id.search);
+        search.setOnQueryTextListener(this);
+        lv=(ListView)findViewById(R.id.lv);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+        lv.setAdapter(adapter);
+        lv.setTextFilterEnabled(true);//这个属性为true表示listview获得当前焦点的时候，相应用户输入的匹配符，筛选出匹配的
+    }
+
+    @Override
+    public boolean onQueryTextChange(String arg0) {
+        if(arg0.length()!=0){
+            lv.setFilterText(arg0);
+        }else{
+            lv.clearTextFilter();
         }
-        return cursor;
+        return false;
+    }
+    @Override
+    public boolean onQueryTextSubmit(String arg0) {
+        /*******************************************************************************************
+         * add city to database
+         */
+        return false;
     }
 }
