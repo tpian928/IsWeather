1564508f3dae9256124d4dd524180f885eed38b9
tpian928 <TPIAN@wangyifeideMacBook-Air.local>
2015-07-24 21:14:50 +0800

    修改Icon

--- app/src/main/java/com/ctc/isweather/control/Icon.java
+++ app/src/main/java/com/ctc/isweather/control/Icon.java
@@ -1,5 +1,7 @@
 package com.ctc.isweather.control;
 
+import android.util.Log;
+
 import com.ctc.isweather.R;
 
 /**
@@ -13,36 +15,37 @@ public class Icon {
      * @return 如果存在则有类似 @drawable/sunny_day 这样的东西，不存在则返回null
      */
     public static int getWeatherIcon(String weatherDesc){
-        int icon = 0;
 
         if (weatherDesc.equals("晴")){
-            icon = R.drawable.sunny_day;
-        }
-        else if (weatherDesc.indexOf("阴")>0||weatherDesc.indexOf("云")>0){
-            icon = R.drawable.cloudy_day;
+            return R.drawable.sunny_day;
         }
         else if (weatherDesc.equals("夜晚多云")){
-            icon = R.drawable.cloudy_night;
+            return R.drawable.cloudy_night;
         }
         else if (weatherDesc.equals("雾霾")||weatherDesc.equals("雾")||weatherDesc.equals("霾")||weatherDesc.equals("haze")){
-            icon = R.drawable.haze;
+            return R.drawable.haze;
         }
-        else if (weatherDesc.indexOf("雪")>0){
-            icon = R.drawable.snow;
+        else if (weatherDesc.indexOf("雪")>=0){
+            return R.drawable.snow;
         }
         else if (weatherDesc.equals("大雨")){
-            icon = R.drawable.pour;
-        }
-        else if (weatherDesc.indexOf("雨")>0){
-            icon = R.drawable.rain;
+            return R.drawable.pour;
         }
         else if (weatherDesc.equals("夜晚")||weatherDesc.equals("晴朗的夜晚")){
-            icon = R.drawable.sunny_night;
+            return R.drawable.sunny_night;
         }
         else if (weatherDesc.equals("暴雨")||weatherDesc.equals("雷阵雨")){
-            icon = R.drawable.thunder_rain;
+            return R.drawable.thunder_rain;
+        }
+        else if (weatherDesc.indexOf("雨")>=0){
+            return R.drawable.rain;
+        }
+        else if (weatherDesc.indexOf("阴")>=0||weatherDesc.indexOf("多云")>=0){
+            return R.drawable.cloudy_day;
+        }
+        else {
+            return 0;
         }
 
-        return icon;
     }
 }
