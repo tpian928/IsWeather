e68ecb0d97d9aad5fb8db22066674375cae95225
tpian928 <TPIAN@wangyifeideMacBook-Air.local>
2015-07-26 14:42:22 +0800

    同步一个小问题

--- app/src/main/java/com/ctc/isweather/http/WeatherHttp.java
+++ app/src/main/java/com/ctc/isweather/http/WeatherHttp.java
@@ -231,10 +231,14 @@ public class WeatherHttp {
      * @param cityName
      * @return get every 3 hour  weather
      */
-    public static ArrayList<HourWeather> getHourWeather(String cityName) throws UnsupportedEncodingException {
+    public static ArrayList<HourWeather> getHourWeather(String cityName) {
         ArrayList<HourWeather> hourWeathers = new ArrayList<HourWeather>();
 
-        cityName = URLEncoder.encode(cityName,"utf-8");
+        try {
+            cityName = URLEncoder.encode(cityName,"utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
         try {
             URL hukd = new URL("http://v.juhe.cn/weather/forecast3h.php?cityname="+cityName+"&key=d6ac5a3b6054da94df74c0157d65fff8");
